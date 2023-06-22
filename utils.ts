2c37f8e3a62e47a1427ab39e18500b97284e085b
Jhonny <aranajhonny@users.noreply.github.com>
2023-06-22 12:17:58 -0500

    add optional parameters

--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 import { state } from "membrane";
 
 export async function api(method: "GET" | "POST", path: string, body?: object) {
-  return await fetch(`https://api.openai.com/v1/${path}`, {
+  const res = await fetch(`https://api.openai.com/v1/${path}`, {
     method,
     body: JSON.stringify(body),
     headers: {
@@ -9,4 +9,13 @@ export async function api(method: "GET" | "POST", path: string, body?: object) {
       "Content-Type": "application/json",
     },
   });
+
+  if (res.status >= 300) {
+    const error = new Error(await res.text());
+    error["status"] = res.status;
+    throw error;
+  }
+
+  return res;
 }
+ 
